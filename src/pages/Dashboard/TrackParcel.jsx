d5f139e5ce778db0d0467f05bd051390981c1537
mohayminul-bd <mohayminulislam514@gmail.com>
2025-12-29 19:25:47 +0600

    trackparcel

--- src/pages/Dashboard/TrackParcel.jsx
+++ src/pages/Dashboard/TrackParcel.jsx
@@ -4,7 +4,7 @@ const TrackParcel = () => {
   return (
     <div>
       <h2 className="text-red-600 text-4xl p-10 items-center">
-        incomplete module purata dekhayni ata (module 66-9) video
+        incomplete module purata dekhayni ata (module 66-9) video..
       </h2>
     </div>
   );
